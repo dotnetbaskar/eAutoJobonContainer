4a5aed5bf127f3aadbe350afcc5790d8b8412a7c
Baskar <baskar.info@yahoo.com>
2019-09-15 11:10:52 +0530

    2nd level checkin

---
+++ PlanGeneration.BackgroundTask/Configuration/BackgroundTaskSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace PlanGeneration.BackgroundTask.Configuration
+{
+    public class BackgroundTaskSettings
+    {
+        public string ConnectionString { get; set; }
+
+        public string EventBusConnection { get; set; }
+
+        public int GracePeriodTime { get; set; }
+
+        public int CheckUpdateTime { get; set; }
+    }
+}
---
+++ PlanGeneration.BackgroundTask/IntegrationEvents/GracePeriodConfirmedIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Events;
+
+namespace PlanGeneration.BackgroundTask.IntegrationEvents
+{
+    public class GracePeriodConfirmedIntegrationEvent : IntegrationEvent
+    {
+        public int OrderId { get; }
+
+        public GracePeriodConfirmedIntegrationEvent(int orderId) =>
+            OrderId = orderId;
+    }
+}
